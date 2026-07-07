85da88982388fc114acb709cc7f928e237f5f060
alschinnen <134975282+alschinnen@users.noreply.github.com>
2026-07-07 08:43:14 -0400

    Update iFlows - Version 1.0.0

---
+++ IntegrationFlows/UnZip/Replicate_Exchange_Rate_from_SAP_S4HANA_OnPremise_to_Workday/src/main/resources/script/ReceiverLogging.groovy
@@ -0,0 +1,40 @@
+import com.sap.gateway.ip.core.customdev.util.Message;
+import java.util.HashMap;
+import java.lang.StringBuffer;
+
+def Message processData(Message message) {
+    //Body
+    def messageLog = messageLogFactory.getMessageLog(message);
+    
+    def logProperty = message.getProperty("PropertyLogging") as String;
+    def logHeader = message.getProperty("HeaderLogging") as String;
+    def logBody = message.getProperty("BodyLogging") as String;
+    def refernceID = message.getProperty("SAPReferenceId") as String;
+    String content = "";
+    
+    if(messageLog != null) {
+        messageLog.setStringProperty("Logging#1", "Payload");
+        
+        if (logProperty != null && logProperty.equalsIgnoreCase("TRUE")) {
+            def propertyMap = message.getProperties()  as String;
+            content = content + "\n" + "Message Properties" + "\n" + "\n" + propertyMap + "\n"
+        }
+        
+        if (logHeader != null && logHeader.equalsIgnoreCase("TRUE")) {
+            def header = message.getHeaders() as String;
+            content = content + "\n" + "Message Headers" + "\n" + "\n" +  header + "\n"
+        }
+        
+        if (logBody == null || logBody.equalsIgnoreCase("TRUE")) {
+            def body = message.getBody(java.lang.String) as String;
+            content = content + "\n" + "Message Body" + "\n" + "\n" + body + "\n";
+        }
+        
+        if (content.length() > 0) {
+            def attachID  = "Receiver message log for Exchange Rate '" + refernceID + "'"	
+            messageLog.addAttachmentAsString(attachID, content, "text/plain");    
+        }
+    }
+
+    return message;
+}
