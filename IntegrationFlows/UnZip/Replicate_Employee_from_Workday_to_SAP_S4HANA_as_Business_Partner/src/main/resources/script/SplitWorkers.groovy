18a5e550a21e35f39186179d9ba77347e41cfe95
alschinnen <134975282+alschinnen@users.noreply.github.com>
2026-07-07 11:26:01 -0400

    Delete IntegrationFlows/UnZip directory

--- IntegrationFlows/UnZip/Replicate_Employee_from_Workday_to_SAP_S4HANA_as_Business_Partner/src/main/resources/script/SplitWorkers.groovy
+++
@@ -1,7 +0,0 @@
-import com.sap.it.api.mapping.*;
-
-def void splitWorkers(String[] input, Output output, MappingContext context) { 
-    for(item in input[0].split(';')){
-     output.addValue(item);
-    }
-}
--- IntegrationFlows/UnZip/Replicate_Employee_from_Workday_to_SAP_S4HANA_as_Business_Partner/src/main/resources/script/createNode.groovy
+++
@@ -1,8 +0,0 @@
-import com.sap.it.api.mapping.*;
-def void createNode(int[] arg1,Output output,MappingContext context){
-
-for(int i=0;i<arg1[0];i++){
-output.addValue("");
-}
-
-}
